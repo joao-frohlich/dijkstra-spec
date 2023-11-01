a8b5c0fd138c0fd87f140fc62eeb975b119f5517
João Vitor Fröhlich <joao.frohlich@edu.udesc.br>
2023-11-01 17:05:05 -0300

    Adicao de algumas funcoes para auxiliar na verificacao do dijkstra

--- GrafoExemplo4.v
+++ GrafoExemplo4.v
@@ -1,6 +1,10 @@
 (* Usando definições do artigo do Martin Erwig sobre grafos indutivos: *)
 (* https://web.engr.oregonstate.edu/~erwig/papers/InductiveGraphs_JFP01.pdf *)
 
+(* A diferença para o arquivo GrafoExemplo3.v consiste na utilização de proposições *)
+
+(* Brincar com grafos infinitos *)
+
 From Coq Require Import Strings.String.
 From Coq Require Export Bool.Bool.
 From Coq Require Export Arith.Arith.
@@ -117,31 +121,103 @@ Inductive Graph :=
     | Empty : Graph
     | CGraph : Context -> Graph -> Graph.
 
-Infix "&" := CGraph (at level 60, right associativity).
+Declare Scope graph_scope.
+Notation "{ n , s }" := (mkcontext n s) (at level 60) : graph_scope.
+Infix "&" := CGraph (at level 60, right associativity) : graph_scope.
+Open Scope graph_scope.
+
+Definition Empty_Graph g :=
+    match g with
+    | Empty => True
+    | _ => False
+    end.
+
+Fixpoint get_node_context (g : Graph) (u : Node) : option Context :=
+    match g with
+    | Empty => None
+    | {n, s} & g' =>
+        if n =? u then
+        Some ({n, s})
+        else
+        get_node_context g' u
+    end.
+    
+Fixpoint get_node_sucessors (a : Adj) : list Node :=
+    match a with
+    | [] => []
+    | (n, _) :: a' => n :: get_node_sucessors a'
+    end.
+
+Fixpoint get_nodes (g : Graph) : list Node :=
+    match g with
+    | Empty => []
+    | ({n, _}) & g' => n :: (get_nodes g')
+    end.
+
+Fixpoint Adj_In_Nodes (a : Adj) (nodes : list Node) :=
+    match a with
+    | [] => True
+    | (n, _) :: a' => In n nodes /\ Adj_In_Nodes a' nodes
+    end.
+
+Fixpoint Valid_Graph' g nodes :=
+    match g with
+    | Empty => True
+    | {_, s} & g' => Adj_In_Nodes s nodes /\ Valid_Graph' g' nodes
+    end.
+
+Definition Valid_Graph g := Valid_Graph' g (get_nodes g).
+
+Fixpoint Path' (g : Graph) (path nodes : list Node) :=
+    match path with
+    | [] => True
+    | x :: path' => match (head path') with
+                    | None => In x nodes
+                    | Some y => match (get_node_context g x) with
+                                | None => False
+                                | Some ({_, s}) => In y (get_node_sucessors s) /\ Path' g path' nodes
+                                end
+                    end
+    end.
+
+Definition Path (g : Graph) (path : list Node) :=
+    Valid_Graph g /\ Path' g path (get_nodes g).
 
 Definition empty_graph := Empty.
 
 Definition example_graph_1 :=
-    (mkcontext 1 [(3,4); (2,3)]) &
-    (mkcontext 2 [(1,2)]) &
-    (mkcontext 3 []) &
+    {1, [(3,4); (2,3)] } &
+    {2, [(1,2)]} &
+    {3, []} &
     Empty.
 
-Definition is_empty g :=
-    match g with
-    | Empty => true
-    | _ => false
-    end.
-
-Lemma empty_graph_is_empty : is_empty empty_graph = true.
+Lemma empty_graph_is_empty : Empty_Graph empty_graph.
 Proof.
-    auto.
+    unfold empty_graph.
+    reflexivity.
 Qed.
 
+Example example_graph_1_valid : Valid_Graph example_graph_1.
+Proof.
+    unfold example_graph_1.
+    unfold Valid_Graph.
+    simpl.
+    split; auto.
+Qed.
+
+Example example_graph_1_path : Path example_graph_1 [1;2;1;3].
+Proof.
+    unfold Path.
+    split.
+    - apply example_graph_1_valid.
+    - unfold example_graph_1.
+      simpl.
+      repeat (split; auto).
+Qed.
 
 (* Função de mapeamento sobre grafos. Aplica uma função f sobre todos
 os nós de um grafo g *)
-Fixpoint graph_map (f : Context -> Context) (g : Graph) :=
+(* Fixpoint graph_map (f : Context -> Context) (g : Graph) :=
     match g with
     | Empty => Empty
     | c & g' => f c & (graph_map f g')
@@ -157,23 +233,7 @@ Proof.
     rewrite IHg.
     rewrite H.
     auto.
-Qed.
-
-Fixpoint get_node_context (g : Graph) (u : Node) : option Context :=
-  match g with
-  | Empty => None
-  | (mkcontext n s) & g' =>
-    if n =? u then
-      Some (mkcontext n s)
-    else
-      get_node_context g' u
-  end.
-
-Fixpoint get_nodes (g : Graph) : list Node :=
-  match g with
-  | Empty => []
-  | (mkcontext n _) & g' => n :: (get_nodes g')
-  end.
+Qed. *)
 
 (* Função de inserção de nó. Um nó é adicionado sem nenhuma aresta, e apenas é adicionado se ele não existe no grafo *)
 Definition add_node (g : Graph) (x : Node) :=
@@ -181,6 +241,46 @@ Definition add_node (g : Graph) (x : Node) :=
     | None => (mkcontext x []) & g
     | Some _ => g
     end.
+
+Lemma Adj_In_Nodes_cons : forall (l : list Node) (a : Adj) (x : Node),
+  Adj_In_Nodes a l -> Adj_In_Nodes a (x :: l).
+Proof.
+  intros.
+  induction a; auto.
+  destruct a.
+  simpl; simpl in H.
+  destruct H as [H1 H2].
+  split; auto.
+Qed.
+
+Lemma Valid_Graph'_cons : forall (g : Graph) (x : Node),
+  Valid_Graph' g (get_nodes g) -> Valid_Graph' g (x :: get_nodes g).
+Proof.
+  intros g.
+  induction g; auto.
+  intros.
+  destruct c.
+  simpl; simpl in H.
+  destruct H as [H1 H2].
+  split.
+  - apply Adj_In_Nodes_cons.
+    apply H1.
+  -  
+
+  Admitted.
+
+Lemma Valid_Graph_add_node : forall (g : Graph) (x : Node),
+  Valid_Graph g -> Valid_Graph (add_node g x).
+Proof.
+  intros.
+  unfold add_node.
+  destruct (get_node_context g x); auto.
+  unfold Valid_Graph.
+  simpl.
+  split; auto.
+  apply Valid_Graph'_cons.
+  auto.
+Qed.
 
 (* Função para adicionar uma aresta. Ela adiciona o par (w,v) na
 lista de adjacência do nó u *)
@@ -282,6 +382,73 @@ Proof.
   - rewrite <- H. auto.
 Qed.
 
+Check fold_left.
+
+Check app.
+
+Section Fold_List_Left_Recursor.
+  Variables A B : Type.
+  Variable f : list A -> list B -> list A.
+
+  Fixpoint fold_list_left (l : list (list B)) (a0 : list A) {struct l} : list A :=
+    match l with
+    | [] => a0
+    | cons b t => fold_list_left t (f a0 b)
+    end.
+End Fold_List_Left_Recursor.
+
+Check map.
+
+Compute [[1]] ++ [[2]].
+
+Definition append_nat_lists (l1 l2 : list (list nat)) := l1 ++ l2.
+
+Compute (fold_left append_nat_lists [[[1]];[[2;5]]] nil).
+
+Compute (map (cons 1) [[1];[2]]).
+
+(* Funciona semelhante à função de DFS (que não é bem um DFS), porém retorna todos os caminhos sem loops entre u e d no
+grafo g. Dá para provar propriedades como a garantia que todos os caminhos válidos são gerados. Uma ideia para pegar
+todos os caminhos seria gerar todas as combinações de nós sem repetição de tamanhos variados, e selecionar os caminhos
+válidos que tenham o como cabeça e d como final *)
+Program Fixpoint get_paths' (g : Graph) (u d : Node) (to_vis : list Node) {measure (length to_vis)} : list (list Node) :=
+  (* Verifica se o nó atual é o mesmo nó que está sendo procurado pela busca *)
+  if u =? d then [[u]]
+  else
+    (* Move o nó atual da busca para a frente de nós a serem visitados *)
+    let (* 1 *) to_vis' := 
+      set_nat_head to_vis u
+    in
+    (* Salva a lista de nós a serem visitados a partir do nó atual. Só são considerados nós que ainda
+    não foram visitados *)
+    let (* 2 *) suc := match (get_node_context g u) with
+      | None => []
+      | Some (mkcontext _ s) => get_elem_in_list s to_vis
+    end in
+    (* Função auxiliar para chamar a função dfs' recursivamente, pois a necessidade de especificar um
+    nó x impede que isso seja feito na função fold_left *)
+    let (* 3 *) aux (x : Node) :=
+      match (* 1' *) to_vis' with
+      | [] => []
+      | h :: t => map (cons h) (get_paths' g x d t)
+      end
+    in
+    (* Aplicação da função dfs' sobre todos os sucessores do nó atual *)
+    fold_left append_nat_lists (map (* 3' *) aux (* 2' *) suc) [].
+Next Obligation.
+Proof.
+  rename Heq_to_vis' into H.
+  unfold set_nat_head in H.
+  destruct (in_nat_list to_vis u) eqn:E in H.
+  - injection H.
+    apply in_nat_list_iff_In in E.
+    intros H1 H2.
+    rewrite H1.
+    unfold lt.
+    rewrite <- remove_nat_one_length; auto.
+  - rewrite <- H. auto.
+Qed.
+
 
 (* Simplificação da chamada da função de DFS *)
 (* Definition dfs (g: Graph) (o d : Node) := 
@@ -293,10 +460,13 @@ Qed.
 Definition dfs (g : Graph) (o d : Node) :=
   dfs' g o d (get_nodes g).
 
+Definition get_paths (g : Graph) (o d : Node) :=
+  get_paths' g o d (get_nodes g).
+
 (* Exemplo da execução da função de DFS sobre o grafo de exemplo 1 *)
 Compute (dfs example_graph_1 1 2).
 
-Example foo : forall (g : Graph) (o : Node), 
+(* Example foo : forall (g : Graph) (o : Node), 
   g <> Empty -> dfs g o o = true.
 Proof.
   intros. induction g.
@@ -322,7 +492,7 @@ Proof.
       * auto.
       * repeat f_equal.
         exists (mkcontext ()). *)
-Admitted.
+Admitted. *)
 
 (* 
 Exemplo de prova sobre Program Fixpoint
@@ -467,9 +637,43 @@ Definition example_graph_2 :=
   (mkcontext 3 [(2,1)]) &
   Empty.
 
+Compute (get_paths example_graph_2 1 2).
+
 Compute dijkstra example_graph_2 1 3.
 
-Lemma dijkstra_o_o_0 : forall (g : Graph) (o : Node),
+Fixpoint get_edge_weight (a : Adj) (x : Node) : option Weight :=
+  match a with
+  | [] => None
+  | (y,w) :: a' => if x =? y then Some w else get_edge_weight a' x
+  end.
+
+Fixpoint get_path_weight (g : Graph) (inf : Weight) (path : list Node) :=
+  match path with
+  | [] => inf
+  | [x] => 0
+  | x :: path' => match head path' with
+                  | None => inf
+                  | Some y => match (get_node_context g x) with
+                              | None => inf
+                              | Some ({_, s}) => match (get_edge_weight s y) with
+                                               | None => inf
+                                               | Some w => w + get_path_weight g inf path'
+                                               end
+                              end
+                  end
+  end.
+
+Compute (map (get_path_weight example_graph_2 (sum_weights example_graph_2)) (get_paths example_graph_2 1 3)).
+
+Definition get_paths_from_o_to_d (g : Graph) (o d : Node) : list Weight :=
+  map (get_path_weight g (sum_weights g)) (get_paths g o d).
+
+Definition get_min_weight_from_o_to_d (g : Graph) (o d : Node) : Weight :=
+  fold_left min (get_paths_from_o_to_d g o d) (sum_weights g).
+
+Compute (get_min_weight_from_o_to_d example_graph_2 1 3).
+
+(* Lemma dijkstra_o_o_0 : forall (g : Graph) (o : Node),
   In o (get_nodes g) -> dijkstra g o o = 0.
 Proof.
   intros.
@@ -494,6 +698,4 @@ Proof.
         unfold projT2.
         simpl.
       * rewrite Nat.eqb_neq in E. contradiction.
-    +  
-        
-    
+    +   *)
